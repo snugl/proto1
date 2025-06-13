92b4cc5fcde014e4fedbf113ebe2c957d36a4466
S1mon <jaforsi.kozak@gmail.com>
2025-06-13 04:58:48 +0200

    implemented highlighter

---
+++ queries/highlights.scm
@@ -0,0 +1,26 @@
+(iden) @type
+(string) @string
+(number) @number
+
+"put"     @keyword
+"rout"    @keyword
+"seq"     @keyword
+"sub"     @keyword
+"lab"     @keyword
+"jump"    @keyword
+"trans"   @keyword
+"pers"    @keyword
+"debug"   @keyword
+
+"use"     @include
+
+
+"+"  @operator
+"-"  @operator
+"="  @operator
+
+">"  @conditional
+"<"  @conditional
+
+(eos) @delimiter
+(comment) @comment
